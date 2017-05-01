60106527e09afc712054b5fc726285f3ce5a520a
Ruzzum <m.ruizsan@live.com.mx>
2017-05-01 03:36:26 -0500

    Fase inicial

---
+++ BlogMusical/App_Start/BundleConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace BlogMusical
+{
+    public class BundleConfig
+    {
+        
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //To Do: Habilitar esta opcion antes de liberar
+            //BundleTable.EnableOptimizations = false;
+
+            //CSS
+            bundles.Add(new StyleBundle("~/bundles/coreCSS")
+                .Include(
+                    "~/Content/css/bootstrap.min.css",
+                    "~/Content/css/font-awesome.min.css",
+                    "~/Content/css/blog-custom.css"
+                ));
+
+            //JS
+            bundles.Add(new ScriptBundle("~/bundles/coreJS")
+                .Include(
+                    "~/Content/js/plugins/jquery-3.1.1.min.js",
+                    "~/Content/js/plugins/bootstrap.min.js",
+                    "~/Content/js/blog-scripts.js"
+                ));   
+        }
+
+    }
+}
---
+++ BlogMusical/Content/js/blog-scripts.js
@@ -0,0 +1,10 @@
+﻿/*
+    JS PARA COMPORTAMIENTO GENERAL DEL BLOG
+    Ultima modificacion: 01/05/2017
+*/
+$(document).ready(function () {
+
+    //To Do: Borrar ya que solo es de prueba
+    $('.prueba').css('background', 'red');
+
+});
