381fa70ac13bd7692e2c32ae0d5c239c1d9d0207
EduardoLam1391 <jlam.91@hotmail.com>
2017-05-01 21:54:06 -0500

    Add files via upload

---
+++ BlogMusical/Controllers/AdmnistradorController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogMusical.Controllers
+{
+    public class AdmnistradorController : Controller
+    {
+        #region  "Administracion del Blog"
+        [HttpGet]
+        public ActionResult AdmBlog()
+        {
+            return View();
+        }
+        #endregion
+
+        #region  "Subir Discografias"
+        [HttpGet]
+        public ActionResult UpdateDiscography()
+        {
+            return View();
+        }
+        #endregion
+
+        #region  "Subir Reseñas"
+        [HttpGet]
+        public ActionResult UpdateReview()
+        {
+            return View();
+        }
+        #endregion
+
+        #region  "Subir Letras de Canciones"
+        [HttpGet]
+        public ActionResult UpdateLyrics()
+        {
+            return View();
+        }
+        #endregion
+
+        #region  "Subir Noticias"
+        [HttpGet]
+        public ActionResult UpdateNews()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
