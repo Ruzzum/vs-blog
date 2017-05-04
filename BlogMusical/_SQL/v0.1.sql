efb1a0d5902aa41ce703cc91bbbeff3b9e52a09e
Ruzzum <m.ruizsan@live.com.mx>
2017-05-04 00:38:12 -0500

    Se agrego carpeta de versiones de SQL

---
+++ BlogMusical/_SQL/v0.1.sql
@@ -0,0 +1,43 @@
+﻿/********************01-05-2017********************/
+create database blog_musical;
+go
+
+create table generos
+(
+	id_genero int identity(1,1) constraint [PK_genero] primary key,
+	nombre varchar(64)
+);
+go
+
+insert into generos
+	   values('Rock'),
+			 ('Hard Rock'),
+			 ('Heavy Metal'),
+			 ('Thrash Metal'),
+			 ('Progressive Rock'),
+			 ('Psychedelic Rock');
+go
+
+create table pais
+(
+	id_pais int identity(1,1) constraint [PK_pais] primary key,
+	nombre varchar(32)
+);
+go
+
+create table usuarios
+(
+	id_usuario int identity(1,1) constraint [PK_usuario] primary key,
+	usuario varchar(12),
+	contrasena varchar(12),
+	nombre varchar(32),
+	apellido_paterno varchar(32),
+	apellido_materno varchar(32),
+	id_pais int,
+	e_mail varchar(32)
+);
+go
+
+alter table usuarios add constraint [FK_usuario_pais] foreign key (id_pais) references pais (id_pais);
+go
+
